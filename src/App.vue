8070bbf48447661f755b45967aa18588fbe14f1d
mikzanco <mikyzancolo97@gmail.com>
2022-11-14 17:21:46 +0100

    fix import and add components

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,17 @@
 <script>
+
+import AppTitle from './components/AppTitle.vue'
+
 export default {
-   name: "App"
+   name: "App",
+   components: {
+    AppTitle
+   }
 }
 </script>
 
 <template>
+  <AppTitle/>
   <div> 
     <h1>Primo Vite</h1>
   </div>
---
+++ src/components/AppTitle.vue
@@ -0,0 +1,19 @@
+<script>
+export default {
+  name:"AppTitle"
+}
+</script>
+
+<template>
+   
+  <h1>La mia prima app con Vite!</h1>
+
+</template>
+
+
+
+<style scoped>
+  h1{
+    color: red;
+  }
+</style>
